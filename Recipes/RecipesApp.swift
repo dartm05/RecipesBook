53b7d23a5fb8aaa801acf6b96b2053c2f8f71130
Daniella Arteaga <darteag@bancolombia.com.co>
2024-11-20 09:09:08 -0500

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Daniella Arteaga on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
